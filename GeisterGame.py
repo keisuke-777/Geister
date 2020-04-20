5d4c2cac65ef5420d7b0989c1d017e11be06ef3e
keisuke <keisuke7771120@me.com>
2020-04-20 10:41:29 +0900

    ゲームの初期値を定義

--- GeisterGame.py
+++ GeisterGame.py
@@ -5,7 +5,7 @@ WidthOfBoard = 6  # ボードの幅
 HeightOfBoard = 6  # 高さ
 MaxNumOfGhost = 8  # プレイヤーの持ち駒の数
 
-MyselfPlayer = 0  # 1番目のプレイヤ(1人称で見た際に自分として扱う)
+MyselfPlayer = 0  # 1番目のプレイヤ(1人称で見た際に自分として扱う)(デフォルト値)
 OpponentPlayer = 1  # 2番目のプレイヤ(敵として扱う)
 NobodyPlayer = -1  # MyselfPlayerでもOpponentPlayerでもない場合に使う値(エラー値)
 
@@ -15,14 +15,30 @@ ColorBlue = 1.0  # 青確定
 ColorUncertain = 0.0  # 完全に不明
 
 
-class Game:
+class Board:
+    """ガイスターの盤面を管理するクラス"""
+
     def __init__(self):
         super().__init__()
+
+
+class Game:
+    """ゲーム全体を管理"""
+
+    def __init__(self):
+        self.players = []  # プレイに参加する2人のプレイヤを保存
+        self.turn_player = MyselfPlayer  # 後手のプレイヤを保存
+        self.first_turn_player = MyselfPlayer  # 先手のプレイヤを保存
+        self.board = Board()  # 盤面(ボードの状態)を保存
+        self.move_num = 0  # 何手目かを保存
 
 
 class GameState:
     def __init__(self):
         super().__init__()
+
+    def game_is_over() -> bool:
+        """ゲームの終了を判定"""
 
 
 def main():
